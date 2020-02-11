5cd5e340c3732beff9069647f6d05f7033f1b325
Ihor Rypka <ihor.rypka@gmail.com>
2020-02-11 19:55:23 +0200

    create custom exceptions for Project Identifier UNIQUE validation rule

--- src/main/java/io/agileintelligence/ppmtool/controller/ProjectController.java
+++ src/main/java/io/agileintelligence/ppmtool/controller/ProjectController.java
@@ -1,16 +1,11 @@
 package io.agileintelligence.ppmtool.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/io/agileintelligence/ppmtool/service/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/service/ProjectService.java
@@ -4,19 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exception.ProjectIdException;
 import io.agileintelligence.ppmtool.repository.ProjectRepository;
 
 @Service
 public class ProjectService {
 	
-	@Autowired
 	private ProjectRepository projectRepository;
+	
+	@Autowired
+	public ProjectService(ProjectRepository projectRepository) {
+		this.projectRepository = projectRepository;
+	}
 	
 	public Project saveOrUpdateProject(Project project) {
 		
-		//Logic
-		
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+			
+		} catch (Exception e) {
+			throw new ProjectIdException("Project ID '" + 
+					project.getProjectIdentifier().toUpperCase() + "' already exists");
+		}
 	}
-
 }
