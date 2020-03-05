3ff84dd11756cdf4acf2c1ba30ff7e15a6c1ad21
Ihor Rypka <ihor.rypka@gmail.com>
2020-03-05 15:09:13 +0200

    set up relationships between Project and Backlog, refactor Projec domain

--- src/main/java/io/agileintelligence/ppmtool/repository/BacklogRepository.java
+++ src/main/java/io/agileintelligence/ppmtool/repository/BacklogRepository.java
@@ -8,4 +8,6 @@ import io.agileintelligence.ppmtool.domain.Backlog;
 @Repository
 public interface BacklogRepository extends CrudRepository<Backlog, Long> {
 
+	Backlog findByProjectIdentifier(String projectIdentifier);
+	
 }
--- src/main/java/io/agileintelligence/ppmtool/service/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/service/ProjectService.java
@@ -3,24 +3,42 @@ package io.agileintelligence.ppmtool.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.agileintelligence.ppmtool.domain.Backlog;
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.exception.ProjectIdException;
+import io.agileintelligence.ppmtool.repository.BacklogRepository;
 import io.agileintelligence.ppmtool.repository.ProjectRepository;
 
 @Service
 public class ProjectService {
 	
 	private ProjectRepository projectRepository;
+	private BacklogRepository backlogRepository;
 	
 	@Autowired
-	public ProjectService(ProjectRepository projectRepository) {
+	public ProjectService(ProjectRepository projectRepository,
+							BacklogRepository backlogRepository) {
 		this.projectRepository = projectRepository;
+		this.backlogRepository = backlogRepository;
 	}
 	
 	public Project saveOrUpdateProject(Project project) {
 		
 		try {
 			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			
+			if (project.getId() == null) {
+				Backlog backlog = new Backlog();
+				project.setBacklog(backlog);
+				backlog.setProject(project);
+				backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			}
+			
+			if (project.getId() != null) {
+				project.setBacklog(backlogRepository.findByProjectIdentifier(
+											project.getProjectIdentifier().toUpperCase()));
+			}
+			
 			return projectRepository.save(project);
 			
 		} catch (Exception e) {
